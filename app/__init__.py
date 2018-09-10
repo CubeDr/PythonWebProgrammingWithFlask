5cb7470b9817ac43b3443e70af2097b0ba3468db
HyunI Kim <spaceship00@naver.com>
2018-09-10 21:14:12 +0900

    글로벌 객체 g
    모듈 단위의 변수를 지원하는 파이썬에서 글로벌 변수를 지원하기 위한 객체.
    
    (문제)
    모듈 내 count 변수를 이용해서 c를 증가시켰을 때는 잘 증가되었다.
    하지만 g.c 를 이용해 증가시키니 제대로 증가되지 않았다.
    몇 번을 호출하든 1이 출력되었다.
    1 2 3 4 ... 증가하며 출력되어야 할 것 같은데 왜 그럴까?

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, g
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    c = getattr(g, 'c', 0)
+    return 'Hello world! {}'.format(c)
+
+
+@app.before_request
+def increase_count():
+    c = getattr(g, 'c', 0)
+    g.c = c + 1
