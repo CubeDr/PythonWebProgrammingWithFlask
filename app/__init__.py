bf12a5b491cbb82699da846c8ad3a25b6a8ea2be
HyunI Kim <spaceship00@naver.com>
2018-09-10 21:52:10 +0900

    응답 4. WSGI 함수 사용
    주로 함수로 선언하여 사용하지만, 클래스로 정의하여 사용하기도 한다.
    보다 정교하게 HTTP 응답을 제어할 수 있어서 고급 응용 프로그램에서는
    자주 이용되는 기법 중 하나.
    하지만 대부분의 경우 다른 방법들로 충분하다.

--- app/__init__.py
+++ app/__init__.py
@@ -34,3 +34,18 @@ def response_string():
 def response_unicode():
     # return make_response(unicode('String response'))
     return u'String response'
+
+
+@app.route('/response/wsgi')
+def response_wsgi():
+    def application(environ, start_response):
+        response_body = 'The request method was %s' % environ['REQUEST_METHOD']
+
+        status = '200 OK'
+        response_headers = [('Content-Type', 'text/plain'),
+                            ('Content-Length', str(len(response_body)))]
+        start_response(status, response_headers)
+
+        return [response_body]
+
+    return make_response(application)
