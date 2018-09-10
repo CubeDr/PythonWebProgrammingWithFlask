39088c8b5db99597e6952fb7f2b292737d19cdc8
HyunI Kim <spaceship00@naver.com>
2018-09-10 21:18:14 +0900

    응답 1. Response class
    Response Class 를 이용하여 커스텀 응답 객체를 생성할 수 있다.
    생성자 매개변수는 앞에서부터 순서대로 response, status, headers.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, g
+from flask import Response, make_response
 
 app = Flask(__name__)
 
@@ -13,3 +14,11 @@ def hello_world():
 def increase_count():
     c = getattr(g, 'c', 0)
     g.c = c + 1
+
+
+@app.route('/response/class')
+def response_class():
+    custom_response = Response('Custom Response', 200, {
+        'Program': 'Flask Web Application'
+    })
+    return make_response(custom_response)
