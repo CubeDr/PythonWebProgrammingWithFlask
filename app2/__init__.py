56fda3f1287e9e6d425b10fda3da3e13ace21b67
HyunI Kim <spaceship00@naver.com>
2018-09-11 22:41:24 +0900

    methods 키워드를 이용하여 요청 메서드에 따라 할 행동을 정의할 수 있다.
    각 요청 메서드별로 다른 함수를 이용할 수 도 있고, 같은 함수에서 한번에
    처리할 수도 있다. 한번에 처리할 경우 request.method 를 이용하여 구분 가능하다.

--- app2/__init__.py
+++ app2/__init__.py
@@ -1,3 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+
+@app.route('/single_method', methods=['GET'])
+def single_method_get():
+    return 'single GET'
+
+
+@app.route('/single_method', methods=['POST'])
+def single_method_post():
+    pass
+
+
+@app.route('/multi_method', methods=['GET', 'POST'])
+def multi_method():
+    if request.method == 'GET':
+        return 'multi GET'
+    else:
+        pass
